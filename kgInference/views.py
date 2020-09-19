6ddbf993021b8be65485c22a25362f20bd59a74b
高成睿 <906678666@qq.com>
2020-09-19 19:22:03 +0800

    update kgInference/views.py. 更新了一个提取数据库数据的函数，输入为省份，科类，年份和专业，返回一个降序的{学校名称：专业分数}的字典，并写了一个简易的testData.html页面进行测试。

--- kgInference/views.py
+++ kgInference/views.py
@@ -1,6 +1,26 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from ormDesign.models import *
 
 # Create your views here.
 def helloworld(request):
     return HttpResponse('Hello World! by kgInference group')
+
+#Get certain major scores in different univ. and rank them
+
+def getMajorScoresRanking(pID,cID,y,mName):
+    #参数为省份ID（整数1-34），科类ID（整数1-3），年份（整数）和专业名称
+    majorList=Majors.objects.filter(provinceID=pID,
+                                    categoryID=cID,
+                                    year=y,
+                                    majorName=mName)
+    scoresDict={}
+    for major in majorList:
+        scoresDict[major.collegeID.collegeName]=major.minScore
+    #return scoresDict
+    scoresOrder=dict(sorted(scoresDict.items(), key = lambda kv:kv[1],reverse=True))
+    return scoresOrder
+
+def testData(request):
+    scoresRanking=getMajorScoresRanking(16,1,2018,"all")
+    return render(request,"testData.html",{"scoresRanking":scoresRanking})
