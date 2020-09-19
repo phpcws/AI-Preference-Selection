a3cc2c57c14602d00ba8335724ee36fdf2b0daca
09118135王屹之 <1624735319@qq.com>
2020-09-19 21:39:40 +0800

    删除文件 kgB/urls.py

--- kgB/urls.py
+++
@@ -1,7 +0,0 @@
-#coding=utf-8
-from django.urls import path
-from . import views
-urlpatterns = [
-    path('', views.helloworld, name='hello'),
-    path('graph/', views.get_json_new, name='graph'),
-]
