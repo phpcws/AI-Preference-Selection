23ee67e4d92241ad7a37d61e4f7e38eae0e6734d
09118135王屹之 <1624735319@qq.com>
2020-09-22 16:52:17 +0800

    update kgB/models.py.

--- kgB/models.py
+++ kgB/models.py
@@ -1,19 +1,14 @@
 from django.db import models
-from py2neo import Graph, Node, Relationship
+from py2neo import Graph, Node, Relationship, NodeMatcher
 from rec2020.settings import graph
 
 # Create your models here.
 
-def get_985s():
-    query = "match (n:`学校`) where n.`_985_`=1 return n"
-    colleges_985 = graph.run(query)
-    return colleges_985
-
-def getProvinceID(province):
+def get_provinceID(province):
     """
-    get province ID
-    :param province: province string; e.g.: '河南'
-    :return: int ID; e.g.: 1
+    获取省份ID
+    :param province: 省份, string; 例如: '河南'
+    :return: 省份ID, int; 例如: 1
     """
     query = "match (n:`省份`) where n.Name='" + str(province)+ "' return n"
     resultDict = dict(graph.run(query).data()[0]['n'])
@@ -21,9 +16,9 @@ def getProvinceID(province):
 
 def get_collegesInProvince(province):
     """
-    get colleges in target province
-    :param province: province string; e.g.: '河南'
-    :return: a list of college info dict
+    获取特定省份内的大学列表
+    :param province: 省份, string; 例如: '河南'
+    :return: 一个 大学信息字典 的列表
     """
     query = "MATCH (n:`学校`)-[:located]-(p:`省份`) WHERE p.Name='" + str(province)+ "' RETURN n"
     collegesInProvince = []
@@ -34,20 +29,21 @@ def get_collegesInProvince(province):
 
 def get_nodelist(province):
     """
-    get nodes (college only 985)
-    :param province: province string; e.g.: '河南'
-    :return: a list of node
+    获取特定省份的节点列表（用于知识图谱可视化）
+    注：此处只获取了省内211院校，而非全部
+    :param province: 省份, string; 例如: '河南'
+    :return: 结点列表
     """
     nodelist = []
     # append college nodes
     collegelist = get_collegesInProvince(province)
     for item in collegelist:
-        if item['_985_']:
+        if item['_211_']:
             nodelist.append(
                 {'data': {'id': str(item['ID']), 'name': item['Name'], 'label': 'college'}}
             )
     # append province node
-    nodelist.append({'data': {'id': str(getProvinceID(province)), 'name': province, 'label': 'province'}})
+    nodelist.append({'data': {'id': str(get_provinceID(province)), 'name': province, 'label': 'province'}})
     # append scoreinfo nodes
     nodelist.append({'data': {'id': '2017w', 'name': '2017文', 'label': 'scoreinfo'}})
     nodelist.append({'data': {'id': '2017l', 'name': '2017理', 'label': 'scoreinfo'}})
@@ -74,3 +70,37 @@ def get_relationshiplist(nodelist):
     for item in scoreinfo_list:
         relationshiplist.append({'data': {'source': str(item['data']['id']), 'target': str(provinID), 'relationship':'score_province'}})
     return relationshiplist
+
+def get_provincedict():
+    """
+    获得 省份ID-省份 的对应字典
+    :return: 省份ID-省份 字典, dict; 例如:{"1": "北京", "2": "天津", "3":"江苏"}
+    """
+    query = "MATCH (n:`省份`) RETURN n"
+    data = graph.run(query).data()
+    provincedict = dict()
+    for item in data:
+        d = dict(item['n'])
+        provincedict[str(d['ID'])] = d['Name']
+    return provincedict
+
+def get_ranktable(provinceID, year, category):
+    """
+    获取一分一段表
+    :param provinceID: 省份编号, int
+    :param year: 年份, int
+    :param category: 科类, string
+    :return: 一分一档字典, dict; 例如:{700: 20, 695: 26, 690: 38, 685: 50, ...}
+    """
+    matcher = NodeMatcher(graph)
+    data = matcher.match("分数信息",
+        provinceID=provinceID,
+        year=year,
+        category=category).all()[0]
+    scoreinfo = dict(data)
+    if(len(scoreinfo['score']) != len(scoreinfo['cumulateNumber'])):
+        raise IndexError
+    ranktable = dict()
+    for score, rank in zip(scoreinfo['score'], scoreinfo['cumulateNumber']):
+        ranktable[score] = rank
+    return ranktable
